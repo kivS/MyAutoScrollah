16b780c6bef29d57efa182ea12d311c4e8d43038
Vik Borges <v1k@protonmail.com>
2017-06-10 20:58:38 +0100

    added typescript

--- src/background.ts
+++ src/background.ts
@@ -1,58 +1,72 @@
 console.log("hey there from background.js");
+
+
 // DB INIT
 var $locations;
-var $db = new loki('db.json', {
+var $db = new loki('db.json',{
     env: 'BROWSER',
     verbose: true,
     autosave: true,
     autoload: true,
-    autoloadCallback: function () {
-        $locations = $db.getCollection('locations');
-        $tracked_pages = $db.getCollection('tracked_pages');
-        // If Collections are not present initialize them
-        if (!$locations)
-            $locations = $db.addCollection('locations');
-        if (!$tracked_pages)
-            $tracked_pages = $db.addCollection('tracked_pages');
-    }
+    autoloadCallback : function(){
+
+        
+       $locations = $db.getCollection('locations');
+       $tracked_pages = $db.getCollection('tracked_pages');
+
+       // If Collections are not present initialize them
+       if(!$locations) $locations = $db.addCollection('locations');
+       if(!$tracked_pages) $tracked_pages = $db.addCollection('tracked_pages');
+
+    },
 });
+
 // Show visual cue  to users when they change to current tab
-chrome.tabs.onActivated.addListener(function (r) {
+chrome.tabs.onActivated.addListener(r => {
     // Get current tab's url
     console.group('Visual Cues');
-    chrome.tabs.query({ active: true }, function (t) {
+   
+    chrome.tabs.query({active:true}, t => {
         var currentTabUrl = t[0].url;
         // clean url
         currentTabUrl = currentTabUrl.split('#')[0];
         console.log('Current Tab url: ', currentTabUrl);
-        if (isPageTracked(currentTabUrl)) {
+
+        if(isPageTracked(currentTabUrl)){
             console.log('Page is being tracked...');
             setBadgeOn();
-        }
-        else {
+
+        }else{
             console.log('Page not tracked...');
             setBadgeOff();
         }
     });
+
     console.groupEnd();
 });
+
+
 // Message station - alllll aboard!
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    //
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	//
     console.group("Incoming messages");
     console.log('Request: ', request);
     console.log('Sender: ', sender);
     console.groupEnd();
     //
+   
     // 
-    switch (request.what) {
-        case "START_BOT":
-            // check If page is already saved
-            var query = $locations.findOne({ url: request.url });
-            if (query) {
+    switch(request.what){
+    	case "START_BOT":
+    		// check If page is already saved
+            var query = $locations.findOne({url: request.url});  
+            if(query){
+                
                 console.group('Saved page query result');
                 console.log('Query: ', query);
                 console.groupEnd();
+
                 // Send back info to be scrolled
                 sendResponse({
                     changes: true,
@@ -62,93 +76,116 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                 setBadgeOn();
                 break;
             }
-            sendResponse({ changes: false });
-            break;
+            
+            sendResponse({changes: false});
+    	break;
+
         case "ECHO_ECHO":
             // Get page location when user leaves tab or closes the page and save it..
             console.group('Page location to be saved');
             console.log('URL: ', request.url);
             console.log('ScrollY: ', request.scrollY);
             console.groupEnd();
+
             savePageLocation(request.url, request.scrollY);
-            break;
-        default:
-            sendResponse({ changes: false });
+
+        break;
+
+    	default:
+    		sendResponse({changes: false});
     }
-});
+
+ });
+
+
 //--------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 /**
  * Saves pages location to the DB
  * @param  {[type]} url        --> url of page
  * @param  {[type]} newScrollY --> new page Y location
- *
+ * 
  */
-function savePageLocation(url, newScrollY) {
+function savePageLocation(url, newScrollY){
     console.group('Save page location');
     // check If page is already saved
-    var query = $locations.findOne({ url: url });
-    if (query && newScrollY != query.scrollY) {
-        console.log('url exists and location is diferent. UPDATE');
-        query.lastScrollY = query.scrollY;
-        query.scrollY = newScrollY;
-        $locations.update(query);
+    var query = $locations.findOne({url: url});
+    if(query && newScrollY != query.scrollY){
+
+       console.log('url exists and location is diferent. UPDATE');
+       query.lastScrollY = query.scrollY;
+       query.scrollY = newScrollY;
+       $locations.update(query);
     }
+
     // If it's a new page && it's being tracked then let's save it
     // 
     var isPageBeingTracked = isPageTracked(url);
-    if (!query && isPageBeingTracked) {
+    if(!query && isPageBeingTracked){
         console.log('A new entry it is. INSERT NEW');
-        $locations.insertOne({ url: url, scrollY: newScrollY, lastScrollY: 0 });
+        $locations.insertOne({url:url, scrollY: newScrollY, lastScrollY: 0});
     }
+
     console.log('isPageBeingTracked: ', isPageBeingTracked);
-    console.groupEnd();
+   
+    console.groupEnd();  
 }
+
 /**
  * Ads page's url to tracked_pages DB
- * @param  {[string]} url
+ * @param  {[string]} url 
  */
-function trackPage(url) {
-    $tracked_pages.insertOne({ page_url: url });
+function trackPage(url){
+    $tracked_pages.insertOne({page_url: url});
     console.group('Track page');
     console.log('tracking: ', url);
     console.groupEnd();
     setBadgeOn();
+    
 }
+
 /**
  * Removes page's url from tracked_pages DB
- * @param  {[url]} url
+ * @param  {[url]} url 
  */
-function untrackPage(url) {
-    $tracked_pages.removeWhere({ page_url: url });
-    $locations.removeWhere({ url: url });
+function untrackPage(url){
+    $tracked_pages.removeWhere({page_url: url});
+    $locations.removeWhere({url: url});
     console.group('Untrack page');
     console.log('untracked: ', url);
     console.groupEnd();
     setBadgeOff();
+
 }
+
 /**
  * Checks $tracked_pages DB to assert if page's being tracked
- * @param  {[url]}  url
- * @return {Boolean}
+ * @param  {[url]}  url 
+ * @return {Boolean}     
  */
-function isPageTracked(url) {
-    var query = $tracked_pages.findOne({ page_url: url });
-    if (query)
-        return true;
+function isPageTracked(url){
+    var query = $tracked_pages.findOne({page_url: url});
+    if(query) return true;
+
     // If page is not being tracked
     return false;
 }
+
 /**
  * Changes badge's text to ON && color to green
  */
-function setBadgeOn() {
-    chrome.browserAction.setBadgeText({ text: 'ON' });
-    chrome.browserAction.setBadgeBackgroundColor({ color: '#2E7D32' });
+function setBadgeOn(){
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    chrome.browserAction.setBadgeBackgroundColor({color: '#2E7D32'});
+
 }
+
 /**
  * Changes badge's text to OFF && color to red
  */
-function setBadgeOff() {
-    chrome.browserAction.setBadgeText({ text: 'OFF' });
-    chrome.browserAction.setBadgeBackgroundColor({ color: '#E53935' });
+function setBadgeOff(){
+    chrome.browserAction.setBadgeText({text: 'OFF'});
+    chrome.browserAction.setBadgeBackgroundColor({color: '#E53935'});
 }
--- src/content.js
+++ src/content.js
@@ -1,144 +1,114 @@
 console.log("Hello world ... not! from content script");
-
 // ## VISIBILITY API BROWSER CONFIG ##
 // 
 // Set the name of the hidden property and the change event for visibility
-var hidden, visibilityChange; 
-if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
-  hidden = "hidden";
-  visibilityChange = "visibilitychange";
-
-} else if (typeof document.msHidden !== "undefined") {
-  hidden = "msHidden";
-  visibilityChange = "msvisibilitychange";
-
-} else if (typeof document.webkitHidden !== "undefined") {
-  hidden = "webkitHidden";
-  visibilityChange = "webkitvisibilitychange";
+var hidden, visibilityChange;
+if (typeof document.hidden !== "undefined") {
+    hidden = "hidden";
+    visibilityChange = "visibilitychange";
 }
-
-
+else if (typeof document.msHidden !== "undefined") {
+    hidden = "msHidden";
+    visibilityChange = "msvisibilitychange";
+}
+else if (typeof document.webkitHidden !== "undefined") {
+    hidden = "webkitHidden";
+    visibilityChange = "webkitvisibilitychange";
+}
 // ##  If user supports visibility API let's added it right?!
 // 
 // Warn if the browser doesn't support addEventListener or the Page Visibility API
 if (typeof document.addEventListener === "undefined" || typeof document[hidden] === "undefined") {
-
-  console.error("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
-
-} else {
-
-  // Handle page visibility change   
-  document.addEventListener(visibilityChange, handleVisibilityChange, false);
+    console.error("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
 }
-
+else {
+    // Handle page visibility change   
+    document.addEventListener(visibilityChange, handleVisibilityChange, false);
+}
 // Handle page unload event   
 window.onbeforeunload = handlePageUnload;
-
-
 // Start up && send some info about page
-chrome.runtime.sendMessage(
-	{
-		what: "START_BOT",
-		scrollY: scrollY,     // info about the Y distance travelled in this page
-		url: getPageUrl()   // current page url
-	},
-	function(response) {
-		console.group('Startup response');
-		console.log('Response: ', response);
-		console.groupEnd();
-
-		if(response.changes){
-			travelY(response.newScrollY, response.oldScrollY);
-		}
-  	}
-);
-
-
+chrome.runtime.sendMessage({
+    what: "START_BOT",
+    scrollY: scrollY,
+    url: getPageUrl() // current page url
+}, function (response) {
+    console.group('Startup response');
+    console.log('Response: ', response);
+    console.groupEnd();
+    if (response.changes) {
+        travelY(response.newScrollY, response.oldScrollY);
+    }
+});
 //---------------------------------------------------------------------------------------------------------------------------------------------
-
 /**
  * Scroll page vertically to determined coord
  * @param  {[integer]} y  --> new coord
- *  
+ *
  */
-function travelY(y, oldY){
-	console.group('TravelY - Scroll page to new Y');
-	console.log('current page Y: ', window.scrollY);
-	console.log('newY: ', y);
-	console.log('oldY: ', oldY);
-
-	// If oldY > 0 and newY is 0 something went down... so let's scroll to olY instead..
-	var scrollY = (y == 0 && oldY > 0) ? oldY:y;
-	console.log('newY or oldY: ', scrollY);
-
-	scrollTo(0, scrollY);
-
-	//make sure it scrolls..
-	forceScroll(scrollY);
-
-	console.groupEnd();
+function travelY(y, oldY) {
+    console.group('TravelY - Scroll page to new Y');
+    console.log('current page Y: ', window.scrollY);
+    console.log('newY: ', y);
+    console.log('oldY: ', oldY);
+    // If oldY > 0 and newY is 0 something went down... so let's scroll to olY instead..
+    var scrollY = (y == 0 && oldY > 0) ? oldY : y;
+    console.log('newY or oldY: ', scrollY);
+    scrollTo(0, scrollY);
+    //make sure it scrolls..
+    forceScroll(scrollY);
+    console.groupEnd();
 }
-
 /**
  * send current page location to be saved
- * 
+ *
  */
-function sendPageLocation(){
-	chrome.runtime.sendMessage(
-		{
-			what: "ECHO_ECHO",
-			scrollY: scrollY,     // info about the Y distance travelled in this page
-			url: getPageUrl()	// current page url
-		}
-	);
+function sendPageLocation() {
+    chrome.runtime.sendMessage({
+        what: "ECHO_ECHO",
+        scrollY: scrollY,
+        url: getPageUrl() // current page url
+    });
 }
-
-
 /**
  * If user changes tab let's record his page position shall we?!
- * 
+ *
  */
-function handleVisibilityChange(){
-	console.group('Page visibility API');
-	console.log('Page visibility: ', document.visibilityState);
-	//document.title = document.visibilityState;
-	console.groupEnd();
-
-	if(document[hidden]){
-		sendPageLocation();
-	}
+function handleVisibilityChange() {
+    console.group('Page visibility API');
+    console.log('Page visibility: ', document.visibilityState);
+    //document.title = document.visibilityState;
+    console.groupEnd();
+    if (document[hidden]) {
+        sendPageLocation();
+    }
 }
-
 /**
  * If user closes page let's save latest location shall we
- * 
+ *
  */
-function handlePageUnload(){
-	sendPageLocation();
+function handlePageUnload() {
+    sendPageLocation();
 }
-
 /**
  * Get current page url
  * @return {[string]} --> filtered url
  */
-function getPageUrl(){
-	return location.origin + location.pathname;
+function getPageUrl() {
+    return location.origin + location.pathname;
 }
-
 /**
  * Force page to scroll..
  * @param  {[type]} y [description]
  * @return {[type]}   [description]
  */
-function forceScroll(y){
-	setTimeout(() => {
-		
-		if(scrollY != y){
-			window.scrollTo(0, y);
-			console.log('I was forced to scroll yo..');
-			// To the rabbit hole we go
-			forceScroll(y);
-		}
-
-	}, 10);
+function forceScroll(y) {
+    setTimeout(function () {
+        if (scrollY != y) {
+            window.scrollTo(0, y);
+            console.log('I was forced to scroll yo..');
+            // To the rabbit hole we go
+            forceScroll(y);
+        }
+    }, 10);
 }
